0aa272c4987e984e287c313952b6eb9bb0a3442b
Gilles Dartiguelongue <eva@gentoo.org>
2015-10-16 10:00:47 +0200

    Add initial unittests

---
+++ time_uuid/test.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+import random
+import unittest
+import uuid
+
+from time_uuid import TimeUUID
+
+
+class TestTimeUUID(unittest.TestCase):
+
+    def test_upgrade(self):
+        self.assertIsNone(TimeUUID.upgrade(None))
+
+        origin = TimeUUID(int=random.randrange(1e7))
+        upgraded = TimeUUID.upgrade(origin)
+        self.assertIsInstance(upgraded, uuid.UUID)
+        self.assertIsInstance(upgraded, TimeUUID)
+        self.assertEqual(str(upgraded), str(origin))
+
+        origin = uuid.uuid1()
+        upgraded = TimeUUID.upgrade(origin)
+        self.assertIsInstance(upgraded, uuid.UUID)
+        self.assertIsInstance(upgraded, TimeUUID)
+        self.assertEqual(str(upgraded), str(origin))
+
+        self.assertRaises(TypeError, TimeUUID.upgrade, 'coucou')
+
+    def test_with_utc(self):
+        time = datetime.datetime.utcnow()
+        res = TimeUUID.with_utc(time, randomize=True)
+
+        self.assertIsInstance(res, uuid.UUID)
+        self.assertIsInstance(res, TimeUUID)
+
+        res1 = TimeUUID.with_utc(time, randomize=False)
+        res2 = TimeUUID.with_utc(time, randomize=False)
+
+        self.assertEqual(res1, res2)
+
+    def test_with_utcnow(self):
+        res = TimeUUID.with_utcnow()
+
+        self.assertIsInstance(res, uuid.UUID)
+        self.assertIsInstance(res, TimeUUID)
